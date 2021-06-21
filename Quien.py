07481ea8ba4f383051cad4e59a69f0e2665052d6
errerero33 <75173531+errerero33@users.noreply.github.com>
2021-06-21 14:10:59 +0200

    Create Quien.py

---
+++ Quien.py
@@ -0,0 +1,30 @@
+Print('Hola! Este és un programa que nos ayudará a saber quién te a preguntado')
+
+input(quién?)
+if
+
+quién? = yo:
+print("Te falta calle")
+
+else 
+quién? = tu:
+print("No que quien te a preguntado")
+
+else
+
+quién? = el:
+print('ok')
+
+else
+
+quién? = nosotros:
+print("just kidding ")
+
+else
+
+quién? = vosotros:
+print('te lo pasas bien')
+
+else 
+quién? = ellos:
+print("lol")
